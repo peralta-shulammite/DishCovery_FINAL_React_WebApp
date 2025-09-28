17a7ae305778f1823c7633caef65a05531a61746
Fatimah Sta. Romana <fatimahstaromana@gmail.com>
2025-09-28 18:54:01 +0800

    change in 4 files in user page, polishing design

--- frontend/library/src/app/user/about-company/page.js
+++ frontend/library/src/app/user/about-company/page.js
@@ -55,47 +55,47 @@ export default function CareersWebApp() {
   ];
 
   const teamMembers = [
-    {
-      name: "Shulammite Peralta",
-      role: "Project Leader & Frontend Developer",
-      image: "https://images.unsplash.com/photo-1494790108755-2616b612b789?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
-      social: {
-        instagram: "#",
-        linkedin: "#",
-        portfolio: "#"
-      }
-    },
-    {
-      name: "Askia Islance Pesa",
-      role: "Backend Developer & Machine Learning Engineer",
-      image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-      social: {
-        instagram: "#",
-        linkedin: "#",
-        portfolio: "#"
-      }
-    },
-    {
-      name: "Fatimah Sta. Romana",
-      role: "Frontend Developer & UX/UI Designer",
-      image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-      social: {
-        instagram: "#",
-        linkedin: "#",
-        portfolio: "#"
-      }
-    },
-    {
-      name: "Samantha Nicole Boado",
-      role: "Backend Developer",
-      image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
-      social: {
-        instagram: "#",
-        linkedin: "#",
-        portfolio: "#"
-      }
+  {
+    name: "Shulammite Peralta",
+    role: "Project Leader & Frontend Developer",
+    image: "/images/team/shulammite-peralta.png",
+    social: {
+      instagram: "#",
+      linkedin: "#",
+      portfolio: "#"
     }
-  ];
+  },
+  {
+    name: "Askia Islance Pesa",
+    role: "Backend Developer & Machine Learning Engineer",
+    image: "/images/team/askia-islance-pesa.jpeg",
+    social: {
+      instagram: "#",
+      linkedin: "#",
+      portfolio: "#"
+    }
+  },
+  {
+    name: "Fatimah Sta. Romana",
+    role: "Frontend Developer & UX/UI Designer",
+    image: "/images/team/fatimah-sta-romana.jpeg",
+    social: {
+      instagram: "#",
+      linkedin: "#",
+      portfolio: "#"
+    }
+  },
+  {
+    name: "Samantha Nicole Boado",
+    role: "Backend Developer",
+    image: "/images/team/samantha-nicole-boado.png",
+    social: {
+      instagram: "#",
+      linkedin: "#",
+      portfolio: "#"
+    }
+  }
+];
 
   return (
     <div ref={aboutTopRef} className="about-container">
